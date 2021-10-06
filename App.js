9cd081c30716c5a16755e74bb7e045fc0fd99e36
0biglife <kimchoon42@gmail.com>
2021-10-07 00:34:30 +0900

    Redux Setting

--- App.js
+++ App.js
@@ -1,33 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  * @flow strict-local
+//  */
 
  
-// import 'react-native-gesture-handler';
-import React, { Component } from 'react';
+// // import 'react-native-gesture-handler';
+// import React, { Component } from 'react';
 
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+// import {StyleSheet, Text, View} from 'react-native';
+// import {NavigationContainer} from '@react-navigation/native';
 
-class App extends Component {
-  render() {
-    return (
-      <View>
-        <Text>hello World</Text>
-      </View>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <NavigationContainer>
+//         <View>
+//           <Text>hello World</Text>
+//         </View>
+//       </NavigationContainer>
+//     );
+//   }
+// }
 
-export default App;
+// const styles = StyleSheet.create({});
+
+// export default App;
---
+++ app/index.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+// import 'react-native-gesture-handler';
+import React, {Component} from 'react';
+
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <View>
+          <Text>hello World</Text>
+        </View>
+      </NavigationContainer>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
