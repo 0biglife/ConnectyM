c02f754ae7a2f23c984b6cb94bda424a8d35168d
0biglife <kimchoon42@gmail.com>
2021-11-01 20:09:10 +0900

    Styled-Component_NewsFeedUI_Changed

--- app/screens/Home/homeView.js
+++ app/screens/Home/homeView.js
@@ -8,7 +8,64 @@ import {
   ActivityIndicator,
   Image,
 } from 'react-native';
+import styled from 'styled-components/native';
 import IonIcon from 'react-native-vector-icons/Ionicons';
+
+const Container = styled.View`
+  background-color: #fff;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+`;
+
+const Card = styled.View`
+  background-color: #f8f8f8;
+  width: 100%;
+  margin-bottom: 20px;
+  border-radius: 10px;
+`;
+
+const UserInfo = styled.View`
+  flex-direction: row;
+  justify-content: flex-start;
+  padding: 15px;
+`;
+
+const UserImg = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+`;
+
+const UserName = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  /* font-family: ; */
+`;
+
+const UserInfoText = styled.View`
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 10px;
+`;
+
+const PostTime = styled.Text`
+  font-size: 12px;
+  color: #666;
+`;
+
+const PostText = styled.Text`
+  font-size: 14px;
+  padding-left: 15px;
+  padding-right: 15px;
+`;
+
+const PostImg = styled.Image`
+  width: 100%;
+  height: 250px;
+  margin-top: 15px;
+`;
 
 const HomeView = () => {
   const [feed, setFeed] = useState([]);
@@ -21,55 +78,74 @@ const HomeView = () => {
       .then(re => {
         setFeed(re.articles);
       });
+    // .catch(function (error) {
+    //   console.log(error.message);
+    //   throw error;
+    // });
   }, []);
 
-  // componentDidMount() {
-  //   this.props.navigation.setOptions({
-  //     headerRight: () => (
-  //       <IonIcon
-  //         name="add"
-  //         size={26}
-  //         color="black"
-  //         style={{
-  //           marginRight: 11,
-  //         }}
-  //         onPress={() => alert('pressed!')}
-  //       />
-  //     ),
-  //   });
-  // }
-
   return (
-    <View style={styles.mainView}>
-      <View style={styles.mainPostView}>
-        {feed.length < 1 ? (
-          <ActivityIndicator size={'large'} color={'#2FBBF0'} />
-        ) : (
-          <FlatList
-            data={feed}
-            kayExtractor={(item, index) => {
-              return item.author.toFixed();
+    <Container>
+      <Card>
+        <UserInfo>
+          <UserImg
+            source={{
+              uri: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/2017_%EA%B8%B0%EB%A6%AC%EB%B3%B4%EC%9D%B4.jpg',
             }}
-            renderItem={({item, index}) => (
-              <View style={styles.postView}>
-                <View style={styles.postTitle}>
-                  <View stlye={styles.ImageView}>
-                    <Image
-                      style={styles.profileImgae}
-                      source={{uri: item.urlToImage}}
-                    />
-                    <Text>{item.author}</Text>
-                  </View>
-                  <View>
-                    <Text>Options</Text>
-                  </View>
-                </View>
-              </View>
-            )}
           />
-        )}
-      </View>
-    </View>
+          <UserInfoText>
+            <UserName>Giriboy</UserName>
+            <PostTime>4 hours ago</PostTime>
+          </UserInfoText>
+        </UserInfo>
+        <PostImg
+          source={{
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/2017_%EA%B8%B0%EB%A6%AC%EB%B3%B4%EC%9D%B4.jpg',
+          }}
+        />
+        <PostText>Hello! I'm new in here!</PostText>
+      </Card>
+    </Container>
+    // <View style={styles.mainView}>
+    //   <View style={styles.mainPostView}>
+    //     {feed.length < 1 ? (
+    //       <ActivityIndicator size={'large'} color={'#2FBBF0'} />
+    //     ) : (
+    //       <FlatList
+    //         data={feed}
+    //         kayExtractor={item => {
+    //           return item.toFixed();
+    //         }}
+    //         renderItem={({item}) => (
+    //           <View style={styles.postView}>
+    //             <View style={styles.postTitle}>
+    //               <View stlye={styles.ImageView}>
+    //                 <Image
+    //                   style={styles.profileImgae}
+    //                   source={{uri: item.urlToImage}}
+    //                 />
+    //                 <View style={styles.titleView}>
+    //                   <Text style={styles.artist_name}>{item.author}</Text>
+    //                 </View>
+    //               </View>
+    //               <View>
+    //                 <IonIcon
+    //                   name="ellipsis-vertical"
+    //                   color="gray"
+    //                   size={16}
+    //                 />
+    //               </View>
+    //             </View>
+    //             <Image
+    //               stlye={styles.postImage}
+    //               source={{ uri: item.urlToImage }}
+    //             />
+    //           </View>
+    //         )}
+    //       />
+    //     )}
+    //   </View>
+    // </View>
   );
 };
 
@@ -79,17 +155,26 @@ const styles = StyleSheet.create({
   },
   mainPostView: {
     width: '100%',
+    // marginBottom: 200,
   },
   postView: {
     width: '90%',
     alignItems: 'center',
-    marginTop: 20,
+    marginTop: 40,
+    backgroundColor: 'gray',
   },
   postTitle: {
     width: '90%',
     display: 'flex',
     justifyContent: 'space-between',
     flexDirection: 'row',
+    backgroundColor: 'pink',
+    alignItems: 'center',
+  },
+  ImageView: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   profileImgae: {
     backgroundColor: 'rgba(0,0,0,0.06)',
@@ -97,10 +182,24 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 50,
   },
-  ImageView: {
-    display: 'flex',
-    flexDirection: 'row',
+  artist_name: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  titleView: {
+    marginLeft: 15,
+    justifyContent: 'center',
+  },
+  postImage: {
+    width: '90%',
+    height: 200,
+    backgroundColor: 'red',
+    marginTop: 20,
+    borderRadius: 10,
   },
 });
 
 export default HomeView;
+
+
+
